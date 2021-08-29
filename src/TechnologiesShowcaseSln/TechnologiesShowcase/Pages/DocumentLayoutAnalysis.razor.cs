10fa66e98c77eaecf93910370809754d2e05ba4e
Eduardo Fonseca <efonsecab@gmail.com>
2021-08-29 14:22:11 -0600

    Adding draft for content analysis using Azure Form Recognizer

---
+++ src/TechnologiesShowcaseSln/TechnologiesShowcase/Pages/DocumentLayoutAnalysis.razor.cs
@@ -0,0 +1,36 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnologiesShowcase.Pages
+{
+    public partial class DocumentLayoutAnalysis
+    {
+        [Inject]
+        private FormRecognizerClient FormRecognizerClient { get; set; }
+        private Response<Azure.AI.FormRecognizer.Models.FormPageCollection> DocumentAnalysisResult { get; set; }
+        private bool IsLoading { get; set; }
+
+        private async Task OnInputFileChange(InputFileChangeEventArgs e)
+        {
+            IsLoading = true;
+            var fileStream = e.File.OpenReadStream();
+            MemoryStream memoryStream = new MemoryStream();
+            await fileStream.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            var operation = await this.FormRecognizerClient.StartRecognizeContentAsync(memoryStream,
+                recognizeContentOptions: new RecognizeContentOptions()
+                {
+                    ReadingOrder = FormReadingOrder.Natural
+                });
+            this.DocumentAnalysisResult = await operation.WaitForCompletionAsync();
+            IsLoading = false;
+        }
+    }
+}
